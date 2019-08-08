7f352dd237b31ee35ac81961f20995fc4a0739a2
Ivan Almer <ivan.almer@ag04.com>
2019-08-08 10:54:38 +0200

    Initial Commit

---
+++ Slagalica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slagalica
+//
+//  Created by Ivan Almer on 08/08/2019.
+//  Copyright © 2019 ialmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
